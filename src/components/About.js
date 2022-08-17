44699ca5b9c772e528408c84eea7702dba1603d7
Peter Jolley <peterjolley456@gmail.com>
2022-08-17 12:43:51 +0930

    changes made for the start of new portfolio

---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const About =() => {
+    return (
+        <div id= "about" style={{paddingTop: '30vh'}}>
+            <div className='columns is-mobile-height'>
+                <div className='column is-1'>
+                </div>
+                <div className='column has text-white '>
+                    <p className='has-text-blue is-size-2 is-bold'>About</p>
+                    <section className='is-size-4'>
+                        <p>Sed blandit libero volutpat sed. Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt. Condimentum id venenatis a condimentum. Nulla posuere sollicitudin aliquam ultrices sagittis orci a. Mauris ultrices eros in cursus turpis massa tincidunt. Cras semper auctor neque vitae tempus quam. Auctor elit sed vulputate mi sit amet mauris commodo. Neque vitae tempus quam pellentesque nec. Sed enim ut sem viverra aliquet eget sit amet
+
+                        </p>
+                        <br/>
+                        <p>Sed blandit libero volutpat sed. Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt. Condimentum id venenatis a condimentum. Nulla posuere sollicitudin aliquam ultrices sagittis orci a. Mauris ultrices eros in cursus turpis massa tincidunt. Cras semper auctor neque vitae tempus quam. Auctor elit sed vulputate mi sit amet mauris commodo. Neque vitae tempus quam pellentesque nec. Sed enim ut sem viverra aliquet eget sit amet
+
+                        </p>
+                        <p>Sed blandit libero volutpat sed. Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt. Condimentum id venenatis a condimentum. Nulla posuere sollicitudin aliquam ultrices sagittis orci a. Mauris ultrices eros in cursus turpis massa tincidunt. Cras semper auctor neque vitae tempus quam. Auctor elit sed vulputate mi sit amet mauris commodo. Neque vitae tempus quam pellentesque nec. Sed enim ut sem viverra aliquet eget sit amet
+
+                        </p>
+                        <br/>
+                        <p>Sed blandit libero volutpat sed. Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt. Condimentum id venenatis a condimentum. Nulla posuere sollicitudin aliquam ultrices sagittis orci a. Mauris ultrices eros in cursus turpis massa tincidunt. Cras semper auctor neque vitae tempus quam. Auctor elit sed vulputate mi sit amet mauris commodo. Neque vitae tempus quam pellentesque nec. Sed enim ut sem viverra aliquet eget sit amet
+                            </p>
+                    </section>
+                </div>
+                <div className='column is-2'>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+export default  About;
---
+++ src/components/Project.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Container,Row} from 'react-bootstrap'
+
+function Project () {
+    return (
+        <Container className='project-section'>
+            <Container>
+                <h1 className='p-heading'>
+                    My Projects 
+                </h1>
+                <Row style = {{justifyContent: 'center', paddingBottom: '10px'}}></Row>
+            </Container>
+        </Container>
+    );
+}
+
+
+export default  Project;
