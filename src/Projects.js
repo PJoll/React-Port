70db5f85f4b21373d918cab94b8c55612dfdc012
Peter Jolley <peterjolley456@gmail.com>
2022-08-30 13:33:05 +0930

    Contact and project adjusted

--- src/Projects.js
+++ src/Projects.js
@@ -10,25 +10,25 @@ export const projects= [
         subtitle: "HTML",
         description:
           "This exciting app generates a password for the user of at least 8 charactors long including the option of special charactors and uppercase characters.",
-        image: "./images/Password.png",
+        image: "./react-portfolio/src/images/Password.png",
         link: " https://pjoll.github.io/Password-Generator/",
-      }
-    //   {
-    //     title: "React Reserve",
-    //     subtitle: "MERN Stack",
-    //     description:
-    //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    //     image: "./project-1.gif",
-    //     link: "https://reactbootcamp.com",
-    //   },
-    //   {
-    //     title: "React Reserve",
-    //     subtitle: "MERN Stack",
-    //     description:
-    //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    //     image: "./project-1.gif",
-    //     link: "https://reactbootcamp.com",
-    //   },
+      },
+      {
+        title: "MovieDex",
+        subtitle: "Html/node",
+        description:
+          "This app using apis enables the user to search any movie and read information and see ratings! It also provides the current streaming services that play the movie.",
+        image: "./",
+        link: "https://gkal14.github.io/MoviDex/",
+      },
+      {
+        title: "React Reserve",
+        subtitle: "MERN Stack",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-1.gif",
+        link: "https://reactbootcamp.com",
+      },
     //   {
     //     title: "React Reserve",
     //     subtitle: "MERN Stack",
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,13 +7,23 @@ export default function Navbar() {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
-            Reed Barger
+           Peter's Portfolio
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#projects" className="mr-5 hover:text-white">
-            Past Work
+            My Projects
           </a>
+          <a href="#about" className="mr-5 hover:text-white">
+            About Me
+          </a>
+          <a href="#contact" className="mr-5 hover:text-white">
+            Contact me
+          </a>
+          <a href="#projects" className="mr-5 hover:text-white">
+            My Projects
+          </a>
+          
         
           
         </nav>
