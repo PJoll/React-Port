e912606f09568b7864bc43d98b35661e8e79054b
Peter Jolley <peterjolley456@gmail.com>
2022-09-01 13:18:20 +0930

    changed contact box

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer()  {
   return (
     <div id='contact' className='text-gray-400 bg-gray-900 body-font flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/ec03594b-259b-462f-a371-4a5355f8de21" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/3e10c187-9d81-4eda-8464-b8b80d858230" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>Submit the form below or email me at - pjoll415@gmail.com </p>
