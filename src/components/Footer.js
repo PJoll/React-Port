f6039d9e3871d86484f5a2667ab0b8ec5e27c528
Peter Jolley <peterjolley456@gmail.com>
2023-02-06 18:14:08 +1030

    Changes to the form in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,44 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faEnvelope,
+  faGithub
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer()  {
   return (
-    <div id='contact' className='text-gray-400 bg-gray-900 body-font flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/3e10c187-9d81-4eda-8464-b8b80d858230" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>Submit the form below or email me at - pjoll415@gmail.com </p>
-            </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Contact me!</button>
-        </form>
+    <><div id='contact' className='text-gray-400 bg-gray-900 body-font flex justify-center items-center p-4'>
+      
     </div>
+    <footer className="text-center text-lg-start bg-light text-muted">
+            <section className="d-flex justify-content-center justify-content-lg-between p-4">
+               
+
+                <div className="text-center p-4">
+                <ul>
+          <li className='w-[150px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] duration-300 bg-blue-600'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="https://tinyurl.com/5n6jwvzs">Linkedin<faLinkedin size={30}/></a>
+          </li>
+          <li className='w-[150px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/FryGit87">Github<faGithub size={30}/></a>
+          </li>
+          <li className='w-[150px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="mailto: kymreilly.87@gmail.com">Email<faEnvelope size={30}/></a>
+          </li>
+          </ul>
+              
+            </div>
+
+
+            </section>
+
+            </footer>
+
+    </>
+        
+
+
   )
 }
 
