ab395f3e9fee0d84ff3f600764812f84e4f2f9d4
Peter Jolley <peterjolley456@gmail.com>
2023-02-06 18:18:52 +1030

    footer changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,13 +18,13 @@ export default function Footer()  {
                 <div className="text-center p-4">
                 <ul>
           <li className='w-[150px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] duration-300 bg-blue-600'>
-            <a className='flex justify-between items-center w-full text-gray-300' href="https://tinyurl.com/5n6jwvzs">Linkedin<faLinkedin size={30}/></a>
+            <a className='flex justify-between items-center w-full text-gray-300' href="">Linkedin<faLinkedin size={30}/></a>
           </li>
           <li className='w-[150px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-            <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/FryGit87">Github<faGithub size={30}/></a>
+            <a className='flex justify-between items-center w-full text-gray-300' href="">Github<faGithub size={30}/></a>
           </li>
           <li className='w-[150px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-            <a className='flex justify-between items-center w-full text-gray-300' href="mailto: kymreilly.87@gmail.com">Email<faEnvelope size={30}/></a>
+            <a className='flex justify-between items-center w-full text-gray-300' href="">Email<faEnvelope size={30}/></a>
           </li>
           </ul>
               
