e6bdb34845b7c6d9a17be1e368108d7c732dead0
Peter Jolley <peter@marango.com.au>
2023-04-18 11:31:45 +0930

    first change to footer and socials

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,13 +18,15 @@ export default function Footer()  {
                 <div className="text-center p-4">
                 <ul>
           <li className='w-[150px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] duration-300 bg-blue-600'>
-            <a className='flex justify-between items-center w-full text-gray-300' href="">Linkedin<faLinkedin size={30}/></a>
+            <a href="https://github.com/PJoll" 
+            className='flex justify-between items-center w-full text-gray-300' >
+              Linkedin<FontAwesomeIcon icon={faLinkedin} size="2x"/></a>
           </li>
           <li className='w-[150px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-            <a className='flex justify-between items-center w-full text-gray-300' href="">Github<faGithub size={30}/></a>
+            <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/PJoll">Github<faGithub size={30}/></a>
           </li>
           <li className='w-[150px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-            <a className='flex justify-between items-center w-full text-gray-300' href="">Email<faEnvelope size={30}/></a>
+            <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/PJoll">Email<faEnvelope size={30}/></a>
           </li>
           </ul>
               
