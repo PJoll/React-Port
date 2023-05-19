eb10095d52a2f66b625035576138e17a391bd6b0
Peter Jolley <peter@marango.com.au>
2023-05-19 13:30:36 +0930

    table now identical the whole way day down

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -26,10 +26,10 @@ export default function Technologies() {
             <li class="px-6 py-2 border-b border-gray-200 w-full">
               JavaScript
             </li>
-            <li class="px-6 py-2 w-full rounded-b-lg">Nodejs</li>
-            <li class="px-6 py-2 w-full rounded-b-lg">Tailwind</li>
-            <li class="px-6 py-2 w-full rounded-b-lg">MongoDB</li>
-            <li class="px-6 py-2 w-full rounded-b-lg">SQL</li>
+            <li class="px-6 py-2 border-b border-gray-200 w-full">Nodejs</li>
+            <li class="px-6 py-2 border-b border-gray-200 w-full">Tailwind</li>
+            <li class="px-6 py-2 border-b border-gray-200 w-full">MongoDB</li>
+            <li class="px-6 py-2 border-b border-gray-200 w-full">SQL</li>
           </ul>
         </div>
       </div>
